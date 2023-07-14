5e4ecccd0a7f244c36d77ab1901daf41d63da094
Harshita <chandaliyaharshita222@gmail.com>
2023-07-14 20:45:48 +0530

    To find sequences of lowercase with underscore

---
+++ Regex_PracticeProblems/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regex_PracticeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regex Practice Problem!");
+            Console.WriteLine("C# program to find sequences of lowercase letters joined by an underscore");
+            string pattern = "^[a-z]{2,}_[a-z]{2,}$";
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Enter lower case input");
+            string s = Console.ReadLine();
+            Console.WriteLine("It is: " + regex.IsMatch(s));
+        }
+    }
+}
